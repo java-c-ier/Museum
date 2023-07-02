2c22e8733f261f4c803e4da2d13ed172b803f901
Jimut <jimutsahoo02@gmail.com>
2023-07-02 16:12:29 +0530

    Deleted

--- app.js
+++
@@ -1,8 +0,0 @@
-img = document.querySelector(".image");
-img.onclick = () => {
-    document.querySelector(".popup").style.display = "block";
-};
-cross = document.querySelector(".contain .popup span");
-cross.onclick = () => {
-    document.querySelector(".popup").style.display = "none";
-};
